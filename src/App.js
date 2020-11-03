778cebc10e823d03a6349e9e59877ba71cbfb20a
pgatic <pgatic@hotmail.com>
2020-11-03 20:27:48 +0100

    add quote component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Quote from './components/quote';
 
 function App() {
+  console.log('app rendering');
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="text-success display-4">Quote machine</h1>
+      <Quote />
     </div>
   );
 }
